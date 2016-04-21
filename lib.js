9a0d9df692cca0e26fd4289fa6aa133b14ec3e19
Alexander Ma[ẏ]orov <alexander.majorov@gmail.com>
2016-04-21 17:34:11 +0300

    Create lib.js

---
+++ lib.js
@@ -0,0 +1,29 @@
+
+/**
+ *
+ */
+export default function addTask(tasks) {
+	if ('requestIdleCallback' in window) {
+    let backgroundTask = deadline => {
+
+			while (deadline.timeRemaining() > 0 && 0 < task.length)
+	      tasks.shift()();
+
+      if (tasks.length > 0) {
+      	console.warn(`Tasks count: ${tasks.length}`);
+				requestIdleCallback(backgroundTask);
+			}
+    };
+
+    requestIdleCallback(backgroundTask);
+  }
+  else if (window.setImmediate) {
+    let c = tasks.length;
+    while (c --> 0)
+      setImmediate(tasks.shift());
+  }
+  else {
+    while (tasks.length)
+      setTimeout(task.shift(), 1);
+  }
+}
